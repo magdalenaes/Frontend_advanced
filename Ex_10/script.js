c4cfe9b0a929913c1979b2db61c097da16bf9e39
Magdalena Sremska <magdalena.sremska@gmail.com>
2020-02-02 23:03:28 +0100

    event handling css & jquery

---
+++ Ex_10/script.js
@@ -0,0 +1,11 @@
+$(".larger").click(function() {
+    $("h1").toggleClass("big");
+});
+
+$(".smaller").click(function() {
+    $("h1").toggleClass("small");
+});
+
+$(".border").click(function() {
+    $("h1").toggleClass("addBorder");
+});
